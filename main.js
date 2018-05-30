5799a3b3d8c71fce83f20675b1af842101c29f34
Li-Hsing Lung <ricky@maestroqa.com>
2018-05-30 12:04:31 -0400

    good luck! :)

---
+++ main.js
@@ -0,0 +1,23 @@
+// TODOs
+// 1. Insert all images into the page
+// 2. Arrange images in a single column in the middle of the page
+// 3. When your mouse hovers over an image, make it double in size
+
+const cats = [
+  'https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr03/4/16/enhanced-26562-1396642697-1.jpg',
+  'https://img.buzzfeed.com/buzzfeed-static/static/2014-04/enhanced/webdr02/9/17/enhanced-buzz-6749-1397079428-5.jpg',
+];
+
+const dogs = [
+  'https://img.buzzfeed.com/buzzfeed-static/static/2015-05/27/12/enhanced/webdr06/enhanced-29622-1432743777-1.jpg',
+  'https://img.buzzfeed.com/buzzfeed-static/static/2015-05/27/11/enhanced/webdr03/enhanced-32739-1432741688-28.jpg',
+];
+
+const alpacas = [
+  'https://cdn.pixabay.com/photo/2017/11/01/11/50/alpaca-2907771_1280.jpg',
+  'https://cdn.pixabay.com/photo/2015/10/13/10/25/alpaca-985838_1280.jpg',
+];
+
+const otters = [
+  'https://cdn.pixabay.com/photo/2017/01/22/17/29/otter-2000284_1280.jpg',
+];
